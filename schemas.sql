afc4862f8f9228f11b027a5f72d82f1d53777f1e
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-05-28 15:46:55 -0300

    Add some equipment schemas

--- schemas.sql
+++ schemas.sql
@@ -12,7 +12,7 @@ CREATE TABLE funcionario(
 	CONSTRAINT pk_funcionario
 		PRIMARY KEY (cpf),
 	CONSTRAINT ck1_funcionario
-		CHECK ( cpf ~ '[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}' ),
+		CHECK ( cpf ~ '^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$' ),
 	CONSTRAINT ck2_funcionario 
 		CHECK ( upper(cargo) IN ('PILOTO', 'COPILOTO', 'ASSISTENTE', 'TECNICO', 'OPCAMERA') )
 );
@@ -79,6 +79,78 @@ CREATE TABLE opcamera (
 		REFERENCES funcionario
 );
 
+
+/* TABELA 7 */
+CREATE TABLE equipamento (
+	id BIGSERIAL,
+	modelo VARCHAR(64) NOT NULL,
+	nome VARCHAR(128) NOT NULL,
+	quantidade INTEGER NOT NULL,
+	marca VARCHAR(32),
+	descricao VARCHAR,
+	tipo CHAR(12) NOT NULL,
+	CONSTRAINT pk_equipamento
+		PRIMARY KEY(id),
+	CONSTRAINT un_equipamento
+		UNIQUE (modelo, nome),
+	CONSTRAINT ck_equipamento
+		CHECK ( upper(tipo) IN ('DRONE', 'CAMERA', 'ESTRUTURACAO', 'SONORIZACAO') )
+);
+
+/* TABELA 8 */
+CREATE TABLE estruturacao (
+	id BIGINT,
+	CONSTRAINT pk_estruturacao
+		PRIMARY KEY (id),
+	CONSTRAINT fk_estruturacao
+		FOREIGN KEY (id)
+		REFERENCES equipamento
+);
+
+/* TABELA 9 */
+CREATE TABLE drone (
+	id BIGINT,
+	fonteAlimentacao VARCHAR(64),
+	tempoMaxVoo INTEGER, /* em minutos */
+	alcanceRemoto INTEGER, /* em metros */
+	CONSTRAINT pk_drone
+		PRIMARY KEY (id),
+	CONSTRAINT fk_drone
+		FOREIGN KEY (id)
+		REFERENCES equipamento
+);
+
+/* TABELA 10 */
+CREATE TABLE sonorizacao (
+	id BIGINT,
+	potencia INTEGER, /* em watts */
+	posseEise BOOLEAN NOT NULL,
+	CONSTRAINT pk_sonorizacao
+		PRIMARY KEY (id),
+	CONSTRAINT fk_sonorizacao
+		FOREIGN KEY (id)
+		REFERENCES equipamento
+);
+
+/* TABELA 11 */
+CREATE TABLE camera (
+	id BIGINT,
+	fonteAlimentacao VARCHAR(64),
+	certificacaoIP VARCHAR(6), /* Pode ser: IP29K, IP291K, IP29 */
+	resistenteQueda BOOLEAN DEFAULT FALSE,
+	visaoNoturna BOOLEAN DEFAULT FALSE,
+	zoomLongoAlcance BOOLEAN DEFAULT FALSE,
+	estabilizadorImagem BOOLEAN DEFAULT FALSE,
+	conectividade VARCHAR(64),
+	CONSTRAINT pk_camera
+		PRIMARY KEY (id),
+	CONSTRAINT fk_camera
+		FOREIGN KEY (id)
+		REFERENCES equipamento,
+	CONSTRAINT ck_camera
+		/* Deve ter 2 ou 3 dígitos em sequencia e talvez terminar com uma letra */
+		CHECK ( certificacaoIP ~ '^IP[0-9]{2,3}[a-zA-Z]{0,1}$' )
+);
 
 
 
