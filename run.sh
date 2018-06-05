76905a216f74f42f1b048fbdb03022f0ce653449
brunoxd <bruno.mendes.costa@usp.br>
2018-06-05 15:55:35 -0300

    Update database install script

--- run.sh
+++ run.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-N=20
+# Variáveis de Ambiente
+NUM_INSERTS=20
 DATABASE=projeto_bd
+TABLE_NAMES=`cat tableNames.txt`
 INSERT_OUT=inserts.sql
 SCHEMA_PATH=sqlFiles/schemas.sql
 GENERATOR_PATH=bllshtMyDatabase/bllshtMyDatabase.py
 DISJOINT_PATH=sqlFiles/disjoint.sql
 OUTPUT=dbDump.sql
 
-# CREATE DATABASE $DATABASE;
-
 # Limpando o banco de dados
-psql $DATABASE -c "DROP SCHEMA public CASCADE;" && \
-psql $DATABASE -c "GRANT ALL ON SCHEMA PUBLIC TO DEV;"
+sudo su -- postgres -c "psql -c \"DROP DATABASE $DATABASE;\""
+sudo su -- postgres -c "psql -c \"CREATE DATABASE $DATABASE;\""
 
 # Criando a estrutura do banco de dados
 psql $DATABASE < $SCHEMA_PATH && \
 
 # Gerando os inserts
-python3 $GENERATOR_PATH $SCHEMA_PATH $N > $INSERT_OUT && \
+python3 $GENERATOR_PATH $SCHEMA_PATH $NUM_INSERTS > $INSERT_OUT && \
 
 # Alimentando o banco de dados com os inserts
 psql $DATABASE < $INSERT_OUT && \
@@ -26,7 +26,10 @@ psql $DATABASE < $INSERT_OUT && \
 # Garante característica de disjoint
 psql $DATABASE < $DISJOINT_PATH
 
+# Limpando o arquivo de saída
+echo -n > $OUTPUT
+
 # Gerando os inserts corrigidos
-for i in `cat tableNames.txt`; do
+for i in $TABLE_NAMES; do	
 	pg_dump --table="$i" --data-only --column-inserts $DATABASE >> $OUTPUT
 done
