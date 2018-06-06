a21d8e922fe69de0238ec84f052f8ff38f0ee52d
brunoxd <bruno.mendes.costa@usp.br>
2018-06-06 18:46:32 -0300

    updated wannaCry

--- wannaCry.sh
+++ wannaCry.sh
@@ -6,6 +6,7 @@ BEGIN_NUM_INSERTS=${1:-1}
 END_NUM_INSERTS=${2:-500}
 NUM_LOOPS=${3:-100}
 OUTPUT_FILENAME=errors.txt
+CLEAN_OUTPUT=cleanErrors.txt
 
 echo -n > $OUTPUT_FILENAME
 
@@ -14,9 +15,13 @@ for i in `seq $BEGIN_NUM_INSERTS $END_NUM_INSERTS`;
 do
 	for j in `seq 1 $NUM_LOOPS`;
 	do		
+		echo "Progress: $i $j"
 		./run.sh $i >> $OUTPUT_FILENAME
+		egrep "DELETE|ROLLBACK|CREATE|DROP|COMMIT|BEGIN|ALTER|INSERT" -v $OUTPUT_FILENAME > $CLEAN_OUTPUT
 	done
 done
 
-echo "Results:"
+echo "Cleaned Outputs:"
+cat $CLEAN_OUTPUT
+echo "Original Results:"
 cat $OUTPUT_FILENAME | sort | uniq -c | sort -k1 -n
