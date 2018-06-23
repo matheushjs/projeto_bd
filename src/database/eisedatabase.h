1da1fd4db02f20ce7cb8efe59c48b1fcdd446fc6
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-06-23 14:05:21 -0300

    Organize method positions

--- src/database/eisedatabase.h
+++ src/database/eisedatabase.h
@@ -18,27 +18,31 @@ public:
 
     QStringList getTableNames();
 
+    /* REPORTS */
     ReportTextData getReport1();
     ReportTextData getReport2();
     ReportTextData getReport3();
     ReportTextData getReport4();
     ReportTextData getReport5();
     ReportTextData getReport6();
+
+    /* SELECTS */
     ReportTextData getSelect1();
+    // Returns information of a single parque
+    // If parque is not found, returns an empty vector
+    StringPairVector selectParque(QString cnpj);
+
+    /* UPDATES */
+    // Updates parque that has the given cnpj with the given information.
+    // Returns an error string, or empty string if successful.
+    QString updateParque(QString cnpj, QString nome, QString mapa, QString endereco);
+
     QString insertCruiseParty(QVector<QString> insertData);
     void commitTransaction();
     void rollbackTransaction();
     StringPairVectorList getEmployeesData();
     StringPairVectorList getEquipmentsData();
     StringPairVectorList getBandsData();
-
-    // Returns information of a single parque
-    // If parque is not found, returns an empty vector
-    StringPairVector selectParque(QString cnpj);
-
-    // Updates parque that has the given cnpj with the given information.
-    // Returns an error string, or empty string if successful.
-    QString updateParque(QString cnpj, QString nome, QString mapa, QString endereco);
 };
 
 #endif // EISEDATABASE_H
