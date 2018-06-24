49c649caa0bebc0fa68465b88954068608056741
brunoxd <bruno.mendes.costa@usp.br>
2018-06-24 18:00:26 -0300

    Update scripts

--- config.sh
+++ config.sh
@@ -2,21 +2,21 @@
 
 echo "Configuring the environment:"
 echo "Checking dependencies..."
-sudo apt-get update -y > logs && \
-sudo apt-get -qq install postgresql postgresql-contrib -y 2>> logs && \
+sudo apt-get update -y > ./logs/log && \
+sudo apt-get -qq install postgresql postgresql-contrib -y 2>> ./logs/log && \
 echo "postgresql OK." && \
 echo "postgresql-contrib OK."
-sudo apt-get -qq install qt5-default -y 2>> logs && \
+sudo apt-get -qq install qt5-default -y 2>> ./logs/log && \
 echo "qt5-default OK."
-sudo apt-get -qq install libqt5sql5-psql -y 2>> logs && \
+sudo apt-get -qq install libqt5sql5-psql -y 2>> ./logs/log && \
 echo "libqt5sql5-psql OK."
-sudo apt-get -qq install python3-numpy -y 2>> logs && \
+sudo apt-get -qq install python3-numpy -y 2>> ./logs/log && \
 echo "python3-numpy OK."
-sudo apt-get -qq install python3-regex -y 2>> logs && \
+sudo apt-get -qq install python3-regex -y 2>> ./logs/log && \
 echo "python3-regex OK."
-sudo apt-get -qq install python3-rstr -y 2>> logs && \
+sudo apt-get -qq install python3-rstr -y 2>> ./logs/log && \
 echo "python3-rstr OK."
-sudo -H pip3 install Faker >> logs && \
+sudo -H pip3 install Faker >> ./logs/log && \
 echo "python3-Faker OK."
 echo "Done."
 
@@ -27,7 +27,7 @@ rm -rf *.o moc* && \
 echo "Done."
 
 echo "Setting up database..."
-./run.sh 50 >> logs
+./run.sh 50 >> ./logs/log
 echo "Done."
 
 echo "Sucessfully configured."
